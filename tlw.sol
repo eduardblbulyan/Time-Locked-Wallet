c3b12ceaf9e393fa7cefd3499a252fd4fa8907c3
Edward <edw.blb@gmail.com>
2024-01-13 02:06:04 +0400

    Adding variables,events and modifiers

--- tlw.sol
+++ tlw.sol
@@ -1,5 +1,20 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 contract TimeLockedWallet{
+    address public owner;
+    uint256 public unlockTime;
+    uint256 balance;
+
+    event Deposit(address indexed sender, uint256 amount);
+    event Withdrawal(address indexed beneficiary, uint256 amount);
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,"You're not the owner");
+        _;
+    }
+    modifier onlyAfter(){
+        require(block.timestamp >= unlockTime,"Not yet");
+        _;
+    }
     
 }
